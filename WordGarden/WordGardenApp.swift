5f7e734b0359eca2c4de1bcffd6825105da9a78b
daniel79438 <daniel79438@gmail.com>
2025-03-10 12:51:55 +0000

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Daniel Harris on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
